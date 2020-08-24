0b5066f999cf28632486bf03ce29b9d1ec866963
max <mihushynmaksym@gmail.com>
2020-08-24 15:00:59 +0300

    Задание №7: Сделать все тесты создания, удаления, модификации для групп и контактов
    Продублированный код убрал в отдельные методы.

--- fixture/contact.py
+++ fixture/contact.py
@@ -2,10 +2,7 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, contact):
-        # fill form new contact
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+    def fill_contact_form(self, contact, wd):
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").send_keys(contact.lastname)
@@ -33,41 +30,20 @@ class ContactHelper:
         wd.find_element_by_name("address2").send_keys(contact.adress2)
         wd.find_element_by_name("phone2").send_keys(contact.phone2)
         wd.find_element_by_name("notes").send_keys(contact.notes)
+
+    def create(self, contact):
+        # create form new contact
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        self.fill_contact_form(contact, wd)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
 
     def modify(self, contact):
         wd = self.app.wd
+        # modify form contact
         wd.find_element_by_xpath("//*[@alt='Edit']").click()
-        # modify form contact
-        wd = self.app.wd
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        # take date from UI
-        wd.find_element_by_xpath("//option[@value='5']").click()
-        wd.find_element_by_xpath("//option[@value='May']").click()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        # take date from UI
-        wd.find_element_by_xpath("(//option[@value='5'])[2]").click()
-        wd.find_element_by_xpath("(//option[@value='May'])[2]").click()
-        # modify form contact
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("address2").send_keys(contact.adress2)
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+        self.fill_contact_form(contact, wd)
         wd.find_element_by_xpath("//*[@value='Update']").click()
         wd.find_element_by_link_text("home page").click()
 
--- fixture/group.py
+++ fixture/group.py
@@ -2,16 +2,18 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    def fill_group(self, group, wd):
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+
     def create(self, group):
         # fill group form
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group(group, wd)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
 
     def modify(self, group):
         # fill group form
@@ -19,11 +21,8 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_xpath("//*[@name='selected[]']").click()
         wd.find_element_by_xpath("//*[@value='Edit group']").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group(group, wd)
         wd.find_element_by_xpath("//*[@name='update']").click()
-        wd.find_element_by_link_text("group page").click()
 
     def delete(self):
         # take first group value and delete
