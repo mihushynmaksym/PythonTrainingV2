7be62d5578035d39035e60faa4e1fa8f67d77beb
max <mihushynmaksym@gmail.com>
2020-10-20 23:58:42 +0300

    Задание №22: Реализовать тесты для добавления контакта в группу и удаления контакта из группы fix6

--- tests/test_address_in_group.py
+++ tests/test_address_in_group.py
@@ -29,18 +29,24 @@ def test_add_contact_in_group(app, db):
         app.group.create(group_value)
     if len(db.get_contact_list()) == 0:
         app.contact.create(contact_value)
-    group = random.choice(db.get_group_list())
-    contact = random.choice(db.get_contact_list())
-    if contact in db.get_contacts_in_group(Group(id='{0}'.format(group.id))):
-        app.contact.create(contact_value)
-        app.group.create(group_value)
-        new_contact = db.get_contact_list()[-1]
-        new_group = db.get_group_list()[-1]
-        app.contact.add_contact_in_group(new_contact.id, new_group.id)
-        assert new_contact in db.get_contacts_in_group(Group(id='{0}'.format(new_group.id)))
+    group = db.get_group_list()
+    contact = db.get_contact_list()
+    group_get_random = random.choice(group)
+    old_contacts_in_group = db.get_contacts_in_group(group_get_random)
+    contacts_is_not_group = []
+    for i in contact:
+        if i not in old_contacts_in_group:
+            contacts_is_not_group.append(i)
+    if len(contacts_is_not_group) != 0:
+        contact_get_random = random.choice(contacts_is_not_group)
+        app.contact.add_contact_in_group(contact_get_random.id, group_get_random.id)
+        old_contacts_in_group.append(contact_get_random)
     else:
-        app.contact.add_contact_in_group(contact.id, group.id)
-        assert contact in db.get_contacts_in_group(Group(id='{0}'.format(group.id)))
+        create_contact = app.contact.create(contact)
+        app.contact.add_contact_in_group(create_contact.id, group_get_random.id)
+        old_contacts_in_group.append(create_contact)
+    new_contacts_in_group = db.get_contacts_in_group(group_get_random)
+    assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
 
 
 def test_dell_contact_in_group(app, db):
@@ -65,10 +71,22 @@ def test_dell_contact_in_group(app, db):
                             phone2='phone219',
                             notes='notes20')
     group_value = Group(name='param1', header='param2', footer='param3')
-    app.contact.create(contact_value)
-    app.group.create(group_value)
-    new_contact = db.get_contact_list()[-1]
-    new_group = db.get_group_list()[-1]
-    app.contact.add_contact_in_group(new_contact.id, new_group.id)
-    app.contact.dell_contact_in_group(new_contact.id, new_group.id)
-    assert new_contact in db.get_contacts_not_in_group(Group(id='{0}'.format(new_group.id)))
+    if len(db.get_group_list()) == 0:  # precondition rule for test
+        app.group.create(group_value)
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(contact_value)
+    group = db.get_group_list()
+    contact = db.get_contact_list()
+    group_get_random = random.choice(group)
+    old_contacts_in_group = db.get_contacts_in_group(group_get_random)
+    if len(old_contacts_in_group) != 0:
+        contact_get_random = random.choice(old_contacts_in_group)
+        app.contact.dell_contact_in_group(contact_get_random.id, group_get_random.id)
+    else:
+        contact_get_random = random.choice(contact)
+        app.contact.add_contact_in_group(contact_get_random.id, group_get_random.id)
+        old_contacts_in_group = db.get_contacts_in_group(group_get_random)
+        app.contact.dell_contact_in_group(contact_get_random.id, group_get_random.id)
+    old_contacts_in_group.remove(contact_get_random)
+    new_contacts_in_group = db.get_contacts_in_group(group_get_random)
+    assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
